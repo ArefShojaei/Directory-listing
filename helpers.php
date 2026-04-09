e2e90dc8ea388ec48a545ba843b006b3b847d915
ArefShojaei <arefshojaei82@gmail.com>
2026-04-10 00:53:41 +0330

    feat: add download & read file helpers

--- helpers.php
+++ helpers.php
@@ -2,35 +2,49 @@
 
 
 function format_file_size(float $bytes, int $decimals = 2): string {
-    $sizes = ['B', 'KB', 'MB', 'GB', 'TB'];
+    $sizes = ["B", "KB", "MB", "GB", "TB"];
 
-    if ($bytes == 0) return '0 B';
+    if ($bytes == 0) return "0 B";
 
     $factor = floor((strlen($bytes) - 1) / 3);
 
-    return sprintf("%.{$decimals}f", $bytes / pow(1024, $factor)) . ' ' . $sizes[$factor];
+    return sprintf("%.{$decimals}f", $bytes / pow(1024, $factor)) . " " . $sizes[$factor];
 }
 
 /**
  * Request processor
  */
-function download(string $file): void {
-    # Securiy headers before downloading the file
-    header('X-Content-Type-Options: nosniff');
-    header('X-Download-Options: noopen');
-    header('X-Frame-Options: DENY');
+function apply_security_headers(): void {
+    header("X-Content-Type-Options: nosniff");
+    header("X-Download-Options: noopen");
+    header("X-Frame-Options: DENY");
     header("Content-Security-Policy: default-src 'none'; frame-ancestors 'none'");
-    header('Referrer-Policy: no-referrer');
-    header('Permissions-Policy: geolocation=(), microphone=(), camera=(), payment=()');
+    header("Referrer-Policy: no-referrer");
+    header("Permissions-Policy: geolocation=(), microphone=(), camera=(), payment=()");
+}
+
+function download_file(string $file): void {
+    apply_security_headers();
 
     # File downloader headers
     header("Content-Type: application/octet-stream");
-    header("Content-Discription: attachment; filename='" . basename($file) . "'");
+    header("Content-Discription: attachment; filename='' . basename($file) . ''");
     header("Expires: 0");
     header("Cache-Control: must-revalidate");
     header("Pragma: public");
     header("Content-Length: " . filesize($file));
     
+    readfile($file);
+
+    exit;
+}
+
+function read_file($file) {
+    # File reader headers
+    header("Content-type: " . mime_content_type($file));
+    header("Content-length: " . filesize($file));
+    header('Cache-Control: public, max-age=86400');
+
     readfile($file);
 
     exit;
